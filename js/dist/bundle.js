d48bff2746de91991d10cdc6509a56db9eb41278
rugead <rudolf.vogel@icando.de>
2017-02-26 15:23:48 +0100

    Update 2017-02-26T14:23:39.189Z

--- js/dist/bundle.js
+++ js/dist/bundle.js
@@ -1,3 +1,6 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
 jQuery(document).ready(function () {
 
     var celsius = 0;
@@ -20,7 +23,7 @@ jQuery(document).ready(function () {
         jQuery.ajax({
             url: url,
             dataType: 'jsonp',
-            success: function (response) {
+            success: function success(response) {
                 console.log('response', response);
                 fahrenheit = response.current_observation.temp_f;
                 celsius = response.current_observation.temp_c;
@@ -33,9 +36,8 @@ jQuery(document).ready(function () {
         });
     }
 
-
     function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
+        console.warn('ERROR(' + err.code + '): ' + err.message);
     }
 
     $("#celsius").click(function () {
@@ -49,5 +51,7 @@ jQuery(document).ready(function () {
     });
 
     navigator.geolocation.getCurrentPosition(success, error, options);
-
 });
+
+},{}]},{},[1])
+//# sourceMappingURL=app.js.map
